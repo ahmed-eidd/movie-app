4e55f754fb36eebb76818809e750154ec6ab7877
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-07-11 14:28:51 +0200

    react router installed

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducers/reducers';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
