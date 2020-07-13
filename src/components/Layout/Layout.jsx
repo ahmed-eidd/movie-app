02ce18085a9678c9f8906b2a538c15c1c59a406d
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-07-13 13:38:31 +0200

    added search bar and signup to layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import classes from './Layout.module.css';
 import Logo from '../UI/Logo/Logo';
 import Nav from '../Nav/Nav';
+import Search from '../Search/Search';
+import SignUp from '../SignUp/SignUp';
+import Spinner from '../UI/Spinner/Spinner';
 
-const Layout = () => {
+const Layout = (props) => {
+  let main = ( <main className={classes.Main}>
+      {props.children}
+    </main>)
+
   return <div className={classes.Layout}>
     <Logo/>
     <Nav/>
+    <Search/>
+    <SignUp/>
+    {main}
   </div>;
 };
 
