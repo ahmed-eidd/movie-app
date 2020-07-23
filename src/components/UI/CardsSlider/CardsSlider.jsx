d7f103f194964af21180edc36011d7159efa07ce
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-07-23 16:08:45 +0200

    added cardsSlider component

---
+++ src/components/UI/CardsSlider/CardsSlider.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import classes from './CardsSlider.module.css';
+import Slider from 'react-slick';
+import PosterCard from '../PosterCard/PosterCard';
+import { Link } from 'react-router-dom';
+
+const CardsSlider = ({ movies, settings, title, link }) => {
+  const genres = useSelector((state) => state.genres);
+  const ImgUrl = (link) => {
+    return `https://image.tmdb.org/t/p/original${link}`;
+  };
+  return (
+    <div className={classes.trendingContainer}>
+      <div className={classes.tredingTitleContainer}>
+        <h2 className={classes.trendingTitle}>{title}</h2>
+
+        <Link to={link} className={classes.SeeAll}>
+          <p>See All</p>
+          <i class="fas fa-long-arrow-alt-right"></i>
+        </Link>
+      </div>
+      <div className={classes.trendingPosterCont}>
+        <Slider {...settings}>
+          {movies.map((movie) => (
+            <PosterCard
+              src={ImgUrl(movie.poster_path)}
+              title={movie.title}
+              key={movie.id}
+              genre={movie.genre_ids.flatMap((g) =>
+                genres
+                  .filter((genre) => genre.id === g)
+                  .map((r) => <li>{r.name},</li>)
+              )}
+            />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default CardsSlider;
