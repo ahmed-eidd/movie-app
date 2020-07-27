188f84ea3d6b17a8b43f081fded40062d841cb50
Ahmed Eid <ahmedeid.webdev@gmail.com>
2020-07-27 21:08:30 +0200

    added ui redux

--- src/store/reducers/UIReducer.js
+++ src/store/reducers/UIReducer.js
@@ -8,6 +8,55 @@ const initialState = {
 
 const UIReducer = (state = initialState, action) =>{ 
   switch(action.type) {
+    case actionTypes.MOVIE_FETCH_START:
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.MOVIE_FETCH_RES:
+      return {
+        ...state,
+        loading:false
+      }
+    case actionTypes.MOVIE_FETCH_ERR:
+      return {
+        ...state,
+        loading:false,
+        error:true
+      }
+     case actionTypes.GENRE_FETCH_START:
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.GENRE_FETCH_RES:
+      return {
+        ...state,
+        loading:false
+      }
+    case actionTypes.GENRE_FETCH_ERROR:
+      return {
+        ...state,
+        loading:false,
+        error:true
+      }
+
+      case actionTypes.CATEGORIES_FECTH_START:
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.CATEGORIES_RES:
+      return {
+        ...state,
+        loading:false
+      }
+    case actionTypes.CATEGORIES_ERR:
+      return {
+        ...state,
+        loading:false,
+        error:true
+      }
     default:
       return state
   }
